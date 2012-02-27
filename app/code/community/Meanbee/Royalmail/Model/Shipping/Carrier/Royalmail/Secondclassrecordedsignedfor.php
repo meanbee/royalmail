8e57ba82a17a0416e55ce830fa3d0c8c93bd9042
Nick Jones <nick@nicksays.co.uk>
2012-02-27 11:26:09 +0000

    Second class, recorded signed for, is 77p extra, not 74p

--- app/code/community/Meanbee/Royalmail/Model/Shipping/Carrier/Royalmail/Secondclassrecordedsignedfor.php
+++ app/code/community/Meanbee/Royalmail/Model/Shipping/Carrier/Royalmail/Secondclassrecordedsignedfor.php
@@ -20,7 +20,7 @@
 class Meanbee_Royalmail_Model_Shipping_Carrier_Royalmail_Secondclassrecordedsignedfor
     extends Meanbee_Royalmail_Model_Shipping_Carrier_Royalmail_Secondclass {
 
-    private $_extraCharge = 0.74;
+    private $_extraCharge = 0.77;
 
     protected function getRates() {
         $rates = parent::getRates();
